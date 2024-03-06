655f05e8d183d34105b3ee12b8f486cd470e3b31
putragabrielll <putragmahk@gmail.com>
2024-03-06 13:09:47 +0700

    fix: register

--- src/helpers/utils.go
+++ src/helpers/utils.go
@@ -1,16 +1,22 @@
 package helpers
 
 import (
-	"net/http"
-	"strings"
 	"github.com/gin-gonic/gin"
 	"github.com/putragabrielll/fwg17-cinematix-be/src/services"
+	"net/http"
+	"strings"
 )
 
 // KHUSUS ERROR HANDLING
 func Utils(err error, ms string, c *gin.Context) {
 	if strings.HasPrefix(err.Error(), "sql: no rows") {
 		c.JSON(http.StatusNotFound, &services.ResponseBack{
+			Success: false,
+			Message: ms,
+		})
+		return
+	} else if strings.HasSuffix(err.Error(), `unique constraint "users_email_key"`) { // Hendler Email Already exists!
+		c.JSON(http.StatusBadRequest, &services.ResponseBack{
 			Success: false,
 			Message: ms,
 		})
