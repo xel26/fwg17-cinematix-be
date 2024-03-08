03e9eee64a27ace65300b3f9e267752507910228
xel26 <zhxo111nvshq@gmail.com>
2024-03-08 21:26:00 +0700

    get movie time

--- src/models/order.models.go
+++ src/models/order.models.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"encoding/base64"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/putragabrielll/fwg17-cinematix-be/src/helpers"
 )
@@ -17,6 +20,15 @@ type Order struct {
 	Total            int    `db:"total" json:"total" form:"total"`
 	MovieTimeId      int    `db:"movieTimeId" json:"movieTimeId" form:"movieTimeId"`
 }
+
+
+
+type MovieTime struct {
+	Id int `db:"id" json:"id"`
+	AiringTimeDate string `db:"airingTimeDate" json:"airingTimeDate"`
+}
+
+
 
 func InsertOrder(c *gin.Context, data Order) (Order, error) {
 	sql := `
@@ -38,4 +50,46 @@ func InsertOrder(c *gin.Context, data Order) (Order, error) {
 	}
 
 	return result, err
+}
+
+
+
+
+func GetMovieTime(c *gin.Context, movieCinemaId int) ([]MovieTime, error) {
+	sql := `
+	SELECT
+	"mt"."id",
+	JSONB_AGG(
+        DISTINCT JSONB_BUILD_OBJECT(
+            'airingTimeDateId', "atd"."id",
+			'airingTime', "at"."time",
+			'date', "d"."date"
+        )
+    ) AS "airingTimeDate"
+	FROM "moviesTime" "mt"
+	JOIN "airingTimeDate" "atd" ON ("atd"."id" = "mt"."airingTimeDateId")
+	JOIN "airingTime" "at" ON ("at"."id" = "atd"."airingTimeId")
+	JOIN "date" "d" ON ("d"."id" = "atd"."dateId")
+	WHERE "mt"."movieCinemaId" = $1
+	GROUP BY "mt"."id"
+	`
+	data := []MovieTime{}
+	err := db.Select(&data, sql, movieCinemaId)
+	if err != nil {
+		return data, err
+	}
+
+	for i := range data{
+		encoded := base64.StdEncoding.EncodeToString([]byte(data[i].AiringTimeDate))
+		decodedVariants, error := base64.StdEncoding.DecodeString(encoded)
+		if error != nil {
+			fmt.Println("decode error", error)
+			return data, error
+		}
+	
+		data[i].AiringTimeDate = string(decodedVariants)
+	}
+
+	
+	return data, err
 }
